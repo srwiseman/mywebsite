bed8e6c071512d346114d53aeaa4c234dba87fa9
Steve Wiseman <swisemanapps@gmail.com>
2018-03-30 14:28:36 -0400

    Created 'about' component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import {slide as Menu} from 'react-burger-menu'
 import FadeIn from 'react-fade-in'
+import About from './components/About'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+
+  handleStateChange (state) {
+    this.setState({menuOpen: state.isOpen})  
+  }
+
+  closeMenu () {
+    console.log("closing...")
+    this.setState({menuOpen: false})
+  }
+
+  toggleMenu () {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
+
   render() {
 
     var menuStyle = {
@@ -13,22 +34,12 @@ class App extends Component {
 
     return (
       <div className="App">
-      <Menu style={menuStyle}>
-      <a id="home" className="menu-item" href="/"><i class="fas fa-question-circle"></i><span>About</span></a>
+      <Menu isOpen={this.state.menuOpen} onStateChange={(state) => this.handleStateChange(state)} style={menuStyle}>
+        <a onClick={() => this.closeMenu()} id="home" className="menu-item" href="#"><i class="fas fa-question-circle"></i><span>About</span></a>
         <a id="about" className="menu-item" href="/about"><i class="fas fa-list-alt"></i><span>Resume</span></a>
         <a id="contact" className="menu-item" href="/contact"><i class="fas fa-at"></i><span>Contact</span></a>
       </Menu>
-      <FadeIn>
-      <div className="content">
-      <FadeIn>
-          <h1 className="App-title">Hello!</h1>
-          <img class='me-pic' src='https://avatars1.githubusercontent.com/u/34013051?s=460&v=4'/>
-          <p>My name's Stephen Wiseman. Mostly, people call me Steve. I'm a computer engineer. I love challenging myself and learning new things. I try to code as much as possible. Whether it's front end or backend, I enjoy it all.
-             When I'm not working or coding, I'm playing sports or traveling!
-          </p>
-      </FadeIn>
-      </div>
-      </FadeIn>
+      <About/>
       </div>
     );
   }
---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import FadeIn from 'react-fade-in'
+
+class About extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+
+  render() {
+
+
+    return (
+    <div>
+      <FadeIn>
+      <div className="content">
+      <FadeIn>
+          <h1 className="App-title">Hello!</h1>
+          <img class='me-pic' src='https://avatars1.githubusercontent.com/u/34013051?s=460&v=4'/>
+          <p>My name's Stephen Wiseman. Mostly, people call me Steve. I'm a computer engineer. I love challenging myself and learning new things. I try to code as much as possible. Whether it's front end or backend, I enjoy it all.
+             When I'm not working or coding, I'm playing sports or traveling!
+          </p>
+      </FadeIn>
+      </div>
+      </FadeIn>
+      </div>
+    );
+  }
+}
+
+export default About;

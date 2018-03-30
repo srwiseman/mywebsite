c4e3557d5cbb875cbaa1de47d114e0c5bb291f3c
Steve Wiseman <swisemanapps@gmail.com>
2018-03-30 14:18:41 -0400

    added my picture. Fixed some layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {slide as Menu} from 'react-burger-menu'
+import FadeIn from 'react-fade-in'
 
 class App extends Component {
   render() {
@@ -17,12 +18,17 @@ class App extends Component {
         <a id="about" className="menu-item" href="/about"><i class="fas fa-list-alt"></i><span>Resume</span></a>
         <a id="contact" className="menu-item" href="/contact"><i class="fas fa-at"></i><span>Contact</span></a>
       </Menu>
+      <FadeIn>
       <div className="content">
+      <FadeIn>
           <h1 className="App-title">Hello!</h1>
+          <img class='me-pic' src='https://avatars1.githubusercontent.com/u/34013051?s=460&v=4'/>
           <p>My name's Stephen Wiseman. Mostly, people call me Steve. I'm a computer engineer. I love challenging myself and learning new things. I try to code as much as possible. Whether it's front end or backend, I enjoy it all.
              When I'm not working or coding, I'm playing sports or traveling!
           </p>
+      </FadeIn>
       </div>
+      </FadeIn>
       </div>
     );
   }

e6e87f65a24fa4ec66a8414594acc2984b7abdb7
Steve Wiseman <swisemanapps@gmail.com>
2018-03-31 16:27:43 -0400

    Added some project info to my projects page.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,9 @@ class Projects extends Component {
       <div className="content">
       <FadeIn>
           <h1 className="App-title">Projects</h1>
+          <p>I'm always working on different coding projects. Whether it's for work, personal, or open source. Here are some of my projects.</p>
+          <p><h2><a href='https://github.com/srwiseman/sounddeez'>Sounddeez</a></h2></p>
+          <p>A music queuing system for Bose WiFi connected radios. Comprised of a backend server written in Golang, and a front end ReactJS server.</p>
       </FadeIn>
       </div>
       </FadeIn>

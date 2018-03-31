5a66f3d3436498a5e3091a5ab1e153485b4e9743
Steve Wiseman <swisemanapps@gmail.com>
2018-03-31 16:18:15 -0400

    Added mycontact info to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,6 +16,9 @@ class Contact extends Component {
       <div className="content">
       <FadeIn>
           <h1 className="App-title">Contact Me!</h1>
+          <p><strong><i class="fas fa-envelope"></i> Email: </strong>: <a href='mailto:swiseman@gmail.com'>swiseman@gmail.com</a></p>
+          <p><strong><i class="fab fa-instagram"></i> Instagram: </strong><a href='https://www.instagram.com/stephenrwiseman/'>stephenrwiseman</a></p>
+          <p><strong><i class="fab fa-git-square"></i> GitHub: </strong><a href='https://github.com/srwiseman'>srwiseman</a></p>
       </FadeIn>
       </div>
       </FadeIn>

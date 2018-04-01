e525deef8243dd9db4167f185e35497f5e08dbe7
Steve Wiseman <swisemanapps@gmail.com>
2018-03-31 22:43:38 -0400

    Added some icons and stuff. Changed the layout a bit

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -16,11 +16,13 @@ class Resume extends Component {
       <div className="content">
       <FadeIn>
           <h1 className="App-title">Resume</h1>
+          <h2><i class="fas fa-briefcase"></i> Work</h2>
           <p><strong>Nokia - Applications Engineer (Team Lead) - 2013 to Current</strong>
           <li>Troubleshoot Java code bugs</li>
           <li>Automate/Build internal tools</li>
           <li>Deploy Software around the world</li>
           </p>
+          <h2><i class="fas fa-graduation-cap"></i> School</h2>
           <p><strong>Memorial University of Newfoundland - 2007 to 2013</strong>
           <li>Bachelor of Computer Engineering</li>
           <li>Completed six work terms</li>

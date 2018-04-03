f5468a95b2b9e87fd0392fdaae0cc7a60bee23fa
Steve Wiseman <swisemanapps@gmail.com>
2018-04-02 22:18:50 -0400

    Fixes #13. Set webpack build to production

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,14 @@ var config = {
         loader : 'babel-loader'
       }
     ]
-  }
+  },
+  plugins : [
+      new webpack.DefinePlugin({
+        "process.env": { 
+          NODE_ENV: JSON.stringify("production") 
+         }
+      })
+  ]
 };
 
 module.exports = config;

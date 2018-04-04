a21d964c116469a9473eaa95cb64240a814386e4
Steve Wiseman <swisemanapps@gmail.com>
2018-04-04 13:14:44 -0400

    Spelling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,7 @@ class Projects extends Component {
       <div className="content">
       <FadeIn>
           <h1 className="App-title">Projects</h1>
-          <p>I'm always working on different coding projects. Whether it's for work, personal, or open source. Here are some of my projects.</p>
+          <p>I try to keep busy! I'm always working on a few different coding projects. Whether it's for work, personal, or open source. Here are some of my projects. Most will have GitHub links so you can look at my code.</p>
           <hr/>
           <p><h2>Sounddeez</h2></p>
           <a href='https://github.com/srwiseman/sounddeez'><strong><i class="fab fa-github-square"></i> GitHub Link</strong></a>
@@ -34,8 +34,7 @@ class Projects extends Component {
           <hr/>
           <p><h2>Triplix</h2></p>
           <p><a href="https://play.google.com/store/apps/details?id=com.swisemanapps.tripli&hl=en"><strong><i class="fab fa-google-play"></i> Google Play</strong></a></p>
-          <p>A game I published on the Apple App Store and the Google Play Store. It's a basic puzzle game written in C#, using the Unity engine. It was featured on the popular site mobile site
-           App Gratis and at one point had 10k active players.</p>
+          <p>A game I published on the Apple App Store and the Google Play Store. It's a basic puzzle game written in C#, using the Unity engine. It was featured on a few popular mobile app sites and at one point had 10k active players.</p>
           <p><strong>Technologies used: </strong> C#, Unity</p>
           <hr/>
           <p><h2>Out East Apparel</h2></p>
@@ -44,6 +43,11 @@ class Projects extends Component {
           We mainly sell t-shirts with Newfoundland-inspired designs.</p>
           <p>Follow us on Social Media!</p>
           <p><a href="https://www.facebook.com/outeastapparel/"><i class="fab fa-facebook-square fa-2x"></i></a> <a href="https://www.instagram.com/outeastapparelnl/"><i class="fab fa-instagram fa-2x"></i></a> <a href="https://twitter.com/outeastapparel"><i class="fab fa-twitter-square fa-2x"></i></a></p>
+          <hr/>
+          <p><h2>Cesium</h2></p>
+          <a href='https://github.com/AnalyticalGraphicsInc/cesium'><strong><i class="fab fa-github-square"></i> GitHub Link</strong></a>
+          <p>I'm a contributor to the open source 3D globe javascript library 'Cesium'. I've fixed bugs, wrote tests, and I'm currently working on a feature to automate some functionality.</p>
+          <p><strong>Technologies used: </strong> Javascript, Jasmine testing framework</p>
       </FadeIn>
       </div>
       </FadeIn>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -22,14 +22,17 @@ class Resume extends Component {
       <FadeIn>
           <h1 className="App-title">Resume</h1>
           <h2><i class="fas fa-briefcase"></i> Work</h2>
-          <p><strong>Nokia - Applications Engineer (Team Lead) - 2013 to Current</strong>
+          <p><h3><strong>Nokia - Applications Engineer (Team Lead) - 2013 to Current</strong></h3>
+          <strong>Responsibilities</strong>
           <br/>Troubleshoot Java code bugs
           <br/>Automate/Build internal tools
           <br/>Deploy Software around the world
+          <br/><strong>Demonstrated Skills:</strong>
+          <br/>Java, SQL, UNIX/Linux, Python, TCP/IP
           </p>
           <h2><i class="fas fa-graduation-cap"></i> School</h2>
-          <p><strong>Memorial University of Newfoundland - 2007 to 2013</strong>
-          <br/>Bachelor of Computer Engineering
+          <p><h3><strong>Memorial University of Newfoundland - 2007 to 2013</strong></h3>
+          Bachelor of Computer Engineering
           <br/>Co-op program (six workterms)
           </p>
       </FadeIn>

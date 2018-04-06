ec6bfbb23b5d41f31d6859b4500a38c8fe38eba2
Steve Wiseman <swisemanapps@gmail.com>
2018-04-05 20:27:08 -0400

    Changed home page. Made it looki likee someone is typing the text.

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FadeIn from 'react-fade-in'
+import Typist from 'react-typist';
 
 class About extends Component {
   constructor(props){
@@ -21,12 +22,22 @@ class About extends Component {
       <div className="content">
       <FadeIn>
           <h1 className="App-title">Hello!</h1>
-          <img class='me-pic' alt='Me!' src='https://avatars1.githubusercontent.com/u/34013051?s=460&v=4'/>
-          <p>My name's Stephen Wiseman. Mostly, people call me Steve. I'm a computer engineer. I love challenging myself and learning new things. I especially love to code.
-             When I'm not working or coding, I'm playing sports or traveling!
-          </p>
-          <p>These days, I'm really interested in coding with <strong>ReactJS</strong>. In fact, this site is made from scratch with <strong>ReactJS</strong> and bundled into a single-page webapp with <strong>webpack</strong>! If you want to look at my code, check out the <strong><a href="https://github.com/srwiseman/mywebsite">GitHub page! </a></strong></p>
+          <p><Typist className="MyTypist" avgTypingDelay={50}>My name's Stephen Wiseman.
+          <Typist.Delay ms={1000} />
+          <br/> But you can call me Steve.
+          <Typist.Delay ms={1000} />
+          <br/> I'm a computer engineer, who loves to code.
+          <Typist.Delay ms={1000} />
+          <br/>
+          These days, I'm really interested in coding with <strong>ReactJS</strong>. 
+          <Typist.Delay ms={1000} />
+          <br/>In fact, this site is made from scratch with <strong>ReactJS</strong>! 
+          <Typist.Delay ms={1000} />
+          <br/>
+          If you want to look at my code, check out the <strong><a href="https://github.com/srwiseman/mywebsite">GitHub page! </a></strong>
+          </Typist>
           <p><a href='https://github.com/srwiseman/'><i class="fab fa-github-square fa-2x"></i></a> <a href='https://www.instagram.com/stephenrwiseman/'><i class="fab fa-instagram fa-2x"></i></a> <a href='https://www.linkedin.com/in/stephen-wiseman-b9120866/'><i class="fab fa-linkedin fa-2x"></i></a></p>
+          </p>
       </FadeIn>
       </div>
       </FadeIn>
